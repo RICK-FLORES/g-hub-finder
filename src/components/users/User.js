fd733d0d627eca7628cfde7b3ce5e4d1031f17ec
RFJ8706 <rf.dev8706@gmail.com>
2021-01-10 14:07:34 -0800

    completed project with out using context api or hooks

---
+++ src/components/users/User.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Spinner from '../layout/Spinner'
+import Repos from '../repos/Repos'
+import PropTypes from 'prop-types'
+
+
+class User extends Component {
+componentDidMount() {
+  this.props.getUser(this.props.match.params.login)
+  this.props.getUserRepos(this.props.match.params.login)
+}
+
+static propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+}
+
+  render() {
+    const { 
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      company,
+    } = this.props.user
+
+    const { loading, repos } = this.props
+
+    if(loading) return <Spinner />
+
+    return (
+      <>
+        <Link to='/' className='btn btn-light'>Go Back</Link>
+        <div className="card grid-2">
+          <div className="all-center">
+            <img 
+              src={avatar_url} 
+              className='round-img' 
+              alt="" 
+              style={{ width: '150px' }} 
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && <>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </>}
+            <a href={html_url} className='btn btn-dark my-1'>View Github</a>
+            <ul>
+              <li>
+                {login && <>
+                  <strong>Username: </strong> {login}
+                </>}
+              </li>
+              <li>
+                {login && <>
+                  <strong>Company: </strong> {company}
+                </>}
+              </li>
+              <li>
+                {login && <>
+                  <strong>Website: </strong> {blog}
+                </>}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Repos: {public_repos}</div>
+          <div className="badge badge-dark">Gists: {public_gists}</div>
+        </div>
+        <h2 style={{textAlign: 'center'}}>Most Recent Repositories</h2>
+        <Repos repos={repos} />
+      </>
+    )
+  }
+}
+
+export default User
