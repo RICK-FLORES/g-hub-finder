8e814db82db777369900a26eaabcf2d560fae263
RFJ8706 <rf.dev8706@gmail.com>
2021-01-11 01:03:24 -0800

    Completed implementation of context. APP v1 Completed.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useContext } from 'react'
+import GithubContext from '../../context/github/githubContext'
+import AlertContext from '../../context/alert/alertContext'
 
 
-const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext)
+  const alertContext = useContext(AlertContext)
+
   const [text, setText] = useState('')
 
   const onSubmit = e => {
     e.preventDefault()
     if(text === '') {
-      setAlert('Please enter something', 'light')
+      alertContext.setAlert('Please enter something', 'light')
     } else {
-      searchUsers(text)
+      githubContext.searchUsers(text)
       setText('')
     }
   }
@@ -40,22 +44,15 @@ const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
         />         
       </form>
       {
-        showClear &&
+        githubContext.users.length > 0 &&
           <button 
             className="btn btn-light btn-block" 
-            onClick={clearUsers}>
+            onClick={githubContext.clearUsers}>
               Clear
             </button>
         }
     </div>
   )
-}
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
 }
 
 export default Search
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,90 +1,87 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
-import PropTypes from 'prop-types'
+import GithubContext from '../../context/github/githubContext'
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext)
+
+  const { user, getUser, loading, getUserRepos, repos } = githubContext
+
   useEffect(() => {
     getUser(match.params.login)
     getUserRepos(match.params.login) 
     // eslint-disable-next-line
-  }, [])
+  }, []) 
 
-    const { 
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      company,
-    } = user
 
-    if(loading) return <Spinner />
+  const { 
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    company,
+  } = user
 
-    return (
-      <>
-        <Link to='/' className='btn btn-light'>Go Back</Link>
-        <div className="card grid-2">
-          <div className="all-center">
-            <img 
-              src={avatar_url} 
-              className='round-img' 
-              alt="" 
-              style={{ width: '150px' }} 
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && <>
-              <h3>Bio</h3>
-              <p>{bio}</p>
-            </>}
-            <a href={html_url} className='btn btn-dark my-1'>View Github</a>
-            <ul>
-              <li>
-                {login && <>
-                  <strong>Username: </strong> {login}
-                </>}
-              </li>
-              <li>
-                {login && <>
-                  <strong>Company: </strong> {company}
-                </>}
-              </li>
-              <li>
-                {login && <>
-                  <strong>Website: </strong> {blog}
-                </>}
-              </li>
-            </ul>
-          </div>
+  if(loading) return <Spinner />
+
+  return (
+    <>
+      <Link to='/' className='btn btn-light'>Go Back</Link>
+      <div className="card grid-2">
+        <div className="all-center">
+          <img 
+            src={avatar_url} 
+            className='round-img' 
+            alt="" 
+            style={{ width: '150px' }} 
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Repos: {public_repos}</div>
-          <div className="badge badge-dark">Gists: {public_gists}</div>
+        <div>
+          {bio && <>
+            <h3>Bio</h3>
+            <p>{bio}</p>
+          </>}
+          <a href={html_url} className='btn btn-dark my-1'>View Github</a>
+          <ul>
+            <li>
+              {login && <>
+                <strong>Username: </strong> {login}
+              </>}
+            </li>
+            <li>
+              {login && <>
+                <strong>Company: </strong> {company}
+              </>}
+            </li>
+            <li>
+              {login && <>
+                <strong>Website: </strong> {blog}
+              </>}
+            </li>
+          </ul>
         </div>
-        <h2 style={{textAlign: 'center'}}>Most Recent Repositories</h2>
-        <Repos repos={repos} />
-      </>
-    )
-  }
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Repos: {public_repos}</div>
+        <div className="badge badge-dark">Gists: {public_gists}</div>
+      </div>
+      <h2 style={{textAlign: 'center'}}>Most Recent Repositories</h2>
+      <Repos repos={repos} />
+    </>
+  )
 }
 
 export default User
